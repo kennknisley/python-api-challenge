433df30c3fbaea4d2913d5869bbc296f009dc5c6
kennknisley <58495936+kennknisley@users.noreply.github.com>
2020-02-29 06:24:29 -0600

    homework update

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "aea2b2436514d99d58b1baf24d52c3df"
+
+# Google API Key
+g_key = "AIzaSyDoZ11D6C7P1Z4VK4WJb7zeutECVqfpKW4"
+
+#Census API Key
+census_key = "040e8717cf4b010a1cbba9d1f7349930182af2ff"
